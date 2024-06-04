23bd04d91f44f317a89eced5dc799a3425174bc6
Gonzalo Lopez <glopezcastro@udesa.edu.ar>
2024-06-04 17:07:08 -0300

    Idea selec

--- code/selection.py
+++ code/selection.py
@@ -1,19 +1,16 @@
 import random
-import team_battle
+import team_combat
 
-def selection():
-    teams = team_battle.fights(50, 400)
-    half = len(teams) // 2
-    # Crear un nuevo diccionario con la mitad de elementos (la mitad superior)
-    half_teams = {k: teams[k] for k in list(teams)[:half]}
+
+def get_teams(number_of_teams: int, number_of_fights):
+    teams = team_combat.fights(number_of_teams, number_of_fights)
+    return teams
+
+def get_probabilities(teams):
+    total_wins = sum(list(teams.values()))
     probability = {}
-    for result in half_teams:
-        probability[str(result.keys())] = round(int(result.values())/400, 2)
-    for number in probability:
-        elected1, elected2 = random.choice(probability), random.choice(probability)
-        veredict = random.random()
-        if float(elected2.values()) > veredict and float(elected1.values()) > veredict:
-            elected1.values()[random.randint(0, 6)], elected2.values()[random.randint(0, 6)] 
+    for team in teams.keys():
+        probability[str(team)] = int(teams[team])/total_wins
+    
 
-selection()
-
+get_probabilities(get_teams(10, 100))
