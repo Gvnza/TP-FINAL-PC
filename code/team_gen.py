9bcc41dff2d04150971efbc46fc52e0962ac7f27
osoreitami5353 <vallejosc@udesa.edu.ar>
2024-06-13 13:04:40 -0300

    Agregado de comentarios I

--- code/team_gen.py
+++ code/team_gen.py
@@ -4,8 +4,7 @@ import utils.pokemon as pokemon
 import utils.move as move
 import utils.team
 
-
-def define_pokemons_objects():
+def define_pokemons_objects() -> Dict[str, pokemon.Pokemon]:
     '''
     Crea todos los pokemones en forma de objetos
 
@@ -14,31 +13,40 @@ def define_pokemons_objects():
     '''
     pokemon_objects = {}
     moves_data = {}
+    # Abre los archivos csv de pokemones y movimientos
     with open('pokemons.csv', newline='') as pokemonfile:
         with open('moves.csv', newline='') as movesfile:
+            # Crea un diccionario con los movimientos
             moves_reader = csv.DictReader(movesfile)
             for row in moves_reader: #Lectura del archivo csv de movimientos
+                # Crea un diccionario con los datos (de cada columna) de movimientos
                 moves_data[row['name']] = {'type' : row['type'], 'category' : row['category'],
                     'pp' : int(row['pp']), 'power' : int(row['power']), 'accuracy' : int(row['accuracy'])}
+        # Crea un diccionario con los pokemones
         pokemon_reader = csv.DictReader(pokemonfile)
         for row in pokemon_reader:
             pokemon_moves = {}
+            # Análisis de si el pokemon es legendario o no
             if row['is_legendary'] != 1:
-                pokemon_info = { #Lectura del archivo csv de pokemones, evito legendarios
+                pokemon_info = { #Lectura del archivo csv de pokemones, evitando legendarios
                     'pokedex_number': row['pokedex_number'], 'type1': row['type1'], 'type2': row['type2'],
                     'hp': int(row['hp']), 'attack': int(row['attack']), 'defense': int(row['defense']),
                     'sp_attack': int(row['sp_attack']), 'sp_defense': int(row['sp_defense']), 'speed': int(row['speed']),
                     'generation': int(row['generation']), 'height_m': row['height_m'], 'weight_kg': row['weight_kg'],
                     'is_legendary': bool(row['is_legendary']), 'moves' : row['moves'].split(';') }
+                
+                # Por cada movimiento dentro de la categoría movimientos...
                 for move in pokemon_info['moves']:
                     if move != '': #Evito que de error si no tiene movimientos (como es el caso de algunos pokemon). Ver nota despues de la funcion
                         pokemon_moves[move] = moves_data[move]
-                pokemon_objects[row['name']] = pokemon.Pokemon.from_dict(row['name'], pokemon_info, pokemon_moves) #Creo los objetos
-    return pokemon_objects
+                
+                # Creación de los objetos de los pokemones
+                pokemon_objects[row['name']] = pokemon.Pokemon.from_dict(row['name'], pokemon_info, pokemon_moves)
+    return pokemon_objects # Retorna los objetos
 
 #Nota: Se podria evitar el if move != '': si se evita que se procesen pokemones del archivo csv que no tengan movimientos, posible cambio a tener en cuenta. -Gonza
 
-def create_teams(cuantity):
+def create_teams(cuantity: int) -> List[utils.team.Team]:
     '''
     Crea todos los equipos.
 
@@ -50,13 +58,19 @@ def create_teams(cuantity):
     '''
     all_pokemon_objects = define_pokemons_objects()
     teams = []
+    # Por cada vez dentro de la cantidad pedida...
     for i in range(cuantity):
         team = []
-        team_pokemon_names = [] #Este codigo no cambio
+        team_pokemon_names = []
+        # Mientras la cantidad de pokemones en el equipo sea menor a 6...
         while len(team) < 6:
+            # Selecciona un nombre de pokemon al azar
             pokemon_name = random.choice(list(all_pokemon_objects.keys()))
+            # Guarda al pokemon en una variable
             pokemon = all_pokemon_objects[pokemon_name]
+            # Si el nombre del pokemon no esta en la lista de nombres de pokemones del equipo...
             if pokemon_name not in team_pokemon_names:
+                # Se agrega el pokemon al equipo y el nombre del pokemon a la lista de nombres de pokemones del equipo
                 team.append(pokemon)
                 team_pokemon_names.append(pokemon_name)
         teams.append(utils.team.Team(str(f'Equipo {i}'), team, 0))
